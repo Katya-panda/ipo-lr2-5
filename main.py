c31ab0606ef3f78add52cdd4cc5ff55454a0b698
Katya <katya_7002@mail.ru>
2024-09-29 19:45:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import math
-def octal_to_decimal(octal_number):
-    decimal_value = 0
-    for index, digit in enumerate(reversed(octal_number)):
-        decimal_value += int(digit) * (8 ** index)
-        return decimal_value
-    def main():
-        octal_input = input("Введите число в восьмеричной системе счисления (5 разрядов): ")
-        if len(octal_input) != 5 or not all (digit in '01234567' for digit in octal_input):
-            print("Ошибка: Введите корректное число в восьмеричной системе счисления из 5 разрядов.")
-            return
-        decimal_output = octal_to_decimal(octal_input)
-        print("Число в десятичной системе счисления: ", decimal_output)
+n = int(input("Введите число в восьмеричнной системе счисления (5 разрядов): "))
+result = 0
+result += int(n[0]) * (8 ** 4)
+result += int(n[1]) * (8 ** 3)
+result += int(n[2]) * (8 ** 2)
+result += int(n[3]) * (8 ** 1)
+result += int(n[40]) * (8 ** 0)
+print(result)
