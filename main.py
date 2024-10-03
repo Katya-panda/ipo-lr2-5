a8d6abfcbae2f95a1441ad73c8e6b5c67d6950aa
Katya <katya_7002@mail.ru>
2024-10-03 12:15:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 n = int(input("Введите число в восьмеричнной системе счисления (5 разрядов): "))
-iflen(n) == 5 and all(char in '01234567' for char in n)
-result = 0
-for i in range(5):
-result += int(n[0]) * (8 ** 4)
-result += int(n[1]) * (8 ** 3)
-result += int(n[2]) * (8 ** 2)
-result += int(n[3]) * (8 ** 1)
-result += int(n[4]) * (8 ** 0)
-print(result)
+eight_last = n % 10
+n //=10
+ten_last = eight_last * (8 ** 0)
+eight_fourth = n % 10
+n //= 10
+ten_fourth = eight_fourth * (8 ** 1)
+eight_ten = n % 10
+n //= 10
+ten_third = e_t * (8 ** 2)
+eight_second = n % 10
+n //= 10
+ten_second = eight_second * (8 ** 3)
+eight_first = n % 10
+n //= 10
+ten_first = eight_first * (8 ** 4)
+sum = ten_last + ten_fourth + ten_third + ten_second + ten_first
+print(f"Число {n} в восьмеричной системе счисления =  число {sum} в десятичной системе счисления")
