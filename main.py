31ae740c473fdd977a4d3e133c75e948d9715282
Katya <katya_7002@mail.ru>
2024-10-03 11:34:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = int(input("Введите число в восьмеричнной системе счисления (5 разрядов): "))
+iflen(n) == 5 and all(char in '01234567' for char in n)
 result = 0
+for i in range(5):
 result += int(n[0]) * (8 ** 4)
 result += int(n[1]) * (8 ** 3)
 result += int(n[2]) * (8 ** 2)
